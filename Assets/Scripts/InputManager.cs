b574b21aea8f3ac573f81d4a786e9d62f884d87f
LouFou2 <parkipos@gmail.com>
2025-11-02 15:08:46 +1100

    touch input (attempt)
    i mean it works, but will have to rework how the camera moves and even how the objects follw.
    Also, for an android build, the compute shader stuff is way too heavy.
    My frame rate for this is atrocious

--- Assets/Scripts/InputManager.cs
+++ Assets/Scripts/InputManager.cs
@@ -1,7 +1,9 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class InputManager : MonoBehaviour
 {
+    private PlayerInput playerInput;
     //This will use the mouse input to move a slime ball
     // It depends on the orientation of the camera how the xy movement should be translated in 3d space
     [SerializeField] private Camera cam;
@@ -22,12 +24,46 @@ public class InputManager : MonoBehaviour
 
     bool controlSuspend;
 
+    private InputAction touchPress;
+    private InputAction touchPosition;
 
+    Vector3 screenPosition;
+
+    private void Awake()
+    {
+        playerInput = GetComponent<PlayerInput>();
+        touchPress = playerInput.actions["TouchPress"];
+        touchPosition = playerInput.actions["Move"];
+    }
+    /*
+    private void OnEnable()
+    {
+        touchPress.performed += TouchPressed;
+    }
+    private void OnDisable()
+    {
+        touchPress.performed -= TouchPressed;
+    }
     
+    private void TouchPressed(InputAction.CallbackContext context)
+    {
+        float value = context.ReadValue<float>();
+        screenPosition = touchPosition.ReadValue<Vector2>();
+        Vector3 addDepth = new Vector3(screenPosition.x, screenPosition.y, cursorDepth);
+        
+        Vector3 touchCamPos = cam.ScreenToWorldPoint(addDepth);
+
+        Debug.Log(touchCamPos);
+
+        objectTargetPos = touchCamPos;
+    }*/
+
     private void Start()
     {
+        /*
         Cursor.lockState = CursorLockMode.Locked;
         Cursor.visible = false;
+        */
     }
 
     void Update()
@@ -35,9 +71,15 @@ public class InputManager : MonoBehaviour
         //suspend control with "e"
         if (Input.GetKey(KeyCode.E)) controlSuspend = !controlSuspend;
 
+        /*
         float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
         float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
         //Debug.Log("mouse x: " + mouseX + "mouse y: " + mouseY);
+
+        // rotating the camera with the mouse
+        targetRotation.x -= mouseY;
+        targetRotation.y += mouseX;
+        
 
         // rotating the camera with the mouse
         targetRotation.x -= mouseY;
@@ -57,17 +99,28 @@ public class InputManager : MonoBehaviour
         Vector3 moveInput = cam.transform.right * moveX + cam.transform.up * moveY + cam.transform.forward * moveZ;
         Vector3 targetMove = moveInput * moveSpeed * Time.deltaTime;
         cam.transform.position += targetMove;
-
+        */
         //use mouse movement for setting the control object's target position
-        objectTargetPos =  cam.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, cam.nearClipPlane + cursorDepth));
+        //objectTargetPos =  cam.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, cam.nearClipPlane + cursorDepth));
+        // Get the current finger position while pressed
+        if (touchPress.IsPressed())
+        {
+            Vector2 screenPos = touchPosition.ReadValue<Vector2>();
 
-        if(!controlSuspend) controllerTransform.position = objectTargetPos;
+            // Convert to world space
+            Vector3 worldPos = cam.ScreenToWorldPoint(new Vector3(screenPos.x, screenPos.y, cursorDepth));
+
+            objectTargetPos = worldPos;
+        }
+
+        if (!controlSuspend) controllerTransform.position = objectTargetPos;
 
         if (Input.GetKey("escape"))
         {
             Application.Quit();
         }
 
+        //cam.transform.LookAt(controllerTransform);
     }
 
     public Vector3 GetControlPointPos()
